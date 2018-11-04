a4b5d06bd6416a74a0651b5b2f6164faff386461
Cameron <cmori007@ucr.edu>
2018-11-03 22:32:52 -0700

    Create base client for test 2

---
+++ client2.py
@@ -0,0 +1,12 @@
+import socket, sys, select, time
+from check import ip_checksum
+
+# create dgram udp socket
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+	print 'Failed to create socket'
+	sys.exit()
+
+host = 'localhost';
+port = 8888;
