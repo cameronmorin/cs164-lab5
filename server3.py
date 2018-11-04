7b464c91111324601763fcd8e723a74215ddf83a
Cameron <cmori007@ucr.edu>
2018-11-03 23:31:44 -0700

    Implement server3 test

--- server3.py
+++ server3.py
@@ -9,10 +9,10 @@ try:
     print 'Socket Created.'
 except socket.error, msg:
     print 'Failed to create socket. Error Code : '+ str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()
+    sys.exit()
 
 try:
-	s.bind((HOST,PORT))
+	s.bind((HOST,port))
 except socket.error, msg:
 	print 'Bind Failed. Error Code : ' + str(msg[0]) + ' Message ' + msg([1])
 	sys.exit()
@@ -26,33 +26,33 @@ currAck = 0
 testCase = 0
 #now keep talking with the client
 while 1:
-	d = s.recvfrom(1025)
-	data = d[0]
-	addr = d[1]
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
 	
-	#set the checksum from from the data recieved
-	checksum = ip_checksum(data[1:-2])
+    #set the checksum from from the data recieved
+    checksum = ip_checksum(data[1:-2])
 
-	if not data:
-		break
+    if not data:
+        break
 	#Wrong ACK
-	if(data[0] != str(currAck)):
-		print 'duplicate'
-		continue
+    if (data[0] != str(currAck)):
+        print 'Duplicate Packet'
+        continue
 	#Corrupted checksum
-	if(data != (str(currAck) + data[1:-2] + checksum)) and (data != (str(not ACKstatus) + data[1: -2] + checkSum)):
-		print 'Corrupt'
-		continue
-	
-    reply = 'ACK' + str(currACK) + ' Message:' + data[1:-2]
-    currAck = 1 - currAck
-	
+    if (data != (str(currAck) + data[1:-2] + checksum)) and (data != (str(not ACKstatus) + data[1: -2] + checkSum)):
+        print 'Corrupt'
+        continue
+
 	#check the test, set a timeout
-	if testCase == 1:
-		time.sleep(14)
-	testCase = testCase + 1
+    if testCase == 1:
+        time.sleep(14)
+    testCase = testCase + 1
 	
+    reply = 'ACK' + str(currAck) + ' Message:' + data[1:-2]
+    currAck = 1 - currAck
+
     s.sendto(reply, addr)
-	print 'Message['+ addr[0] + ':' + str(addr[1]) + '] - ' + data[1:-2]
+    print 'Message['+ addr[0] + ':' + str(addr[1]) + '] - ' + data[1:-2]
 	
 s.close()
