9c91ca321b289f0ff92bee91991285282290a95b
Cameron <cmori007@ucr.edu>
2018-11-03 23:03:29 -0700

    Implemented scenario 2

--- server2.py
+++ server2.py
@@ -19,3 +19,32 @@ except socket.error, msg:
 
 print 'Socket bind complete.'
 
+inputs = [s]
+outputs = []
+timeout = 5
+currACK = 0
+
+while 1:
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+    checksum = ip_checksum(data[1:-2]) # Uses only the raw string of characters
+
+    if not data:
+        break
+    if data[0] != str(currACK):
+        # Repeated ACK
+        print 'Duplicate.'
+        continue
+    if (data != (str(currACK) + data[1:-2] + checksum)) and (data != (str(not currACK) + data[1:-2] + checksum)):
+        # Checksum is corrupted
+        print 'Corrupted data.'
+        continue
+    
+    reply = 'ACK' + str(currACK) + ' Message:' + data[1:-2]
+    currACK = 1 - currACK # 1-1=0 1-0=1
+
+    s.sendto(reply, addr)
+    print 'Message [' + addr[0] + ':' + str(addr[1]) + '] - ' + data [1:-2]
+
+s.close()
